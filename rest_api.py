3d83b733c3bd74927f8eb8d4a65dedc45d056c8e
Theresa <theresa.shan@intel.com>
2019-01-08 11:13:03 +0800

    add server pusher and object count api for chart.
    Signed-off-by: Theresa <theresa.shan@intel.com>

--- rest_api.py
+++ rest_api.py
@@ -1,13 +1,91 @@
-from flask import Flask, jsonify, request, Response, abort
-from flask_pymongo import PyMongo
+import math
+import os
+import pytz
+import time
+import datetime
+from flask import Flask, jsonify, request, Response, abort, send_file
+#from flask_pymongo import PyMongo
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from flask_cors import CORS
 from bson.json_util import dumps
 import traceback
+#from flask_socketio import SocketIO
+#from flask_socketio import SocketIO, emit, send, disconnect
+from flask_sse import sse
+from concurrent.futures import ThreadPoolExecutor
+
+
+executor = ThreadPoolExecutor(1)
 
 # create an Instance of Flask
 app = Flask(__name__, static_url_path='')
-app.config['MONGO_DBNAME'] = 'edge2cloud'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/edge2cloud'
-mongo = PyMongo(app)
+"""random secret key"""
+KEY_SIZE = 32
+SECRET_KEY = os.urandom(KEY_SIZE)
+app.config['SECRET_KEY'] = SECRET_KEY
+MONGO_DB = 'edge2cloud'
+MONGO_URI = 'mongodb://localhost:27017/edge2cloud?replicaSet=rs'
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}, r"/stream": {"origins": "*"}})
+#socketio = SocketIO(app)
+conn = MongoClient(MONGO_URI)
+db = conn[MONGO_DB]
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix="/stream")
+
+
+@app.route("/api/v1/count", methods=['GET'])
+def get_alerts_count():
+    tz_str = request.args.get('tz')
+    # print(tz_str)
+    if not tz_str in pytz.all_timezones:
+        abort(400)
+    time_now = time.time()
+    #start_time = (time_now - 4*60*60) * 1000
+    tz = pytz.timezone(tz_str)
+    dt = datetime.datetime.fromtimestamp(time_now, tz)
+    hour = dt.hour
+    #labels = [ get_hour(hour-4) , get_hour(hour-3), get_hour(hour-2), get_hour(hour-1), hour ]
+    labels = [ get_hour(hour-h) for h in range(4, -1, -1)]
+
+    start_time = 1546504974674
+    pipeline = [
+        {"$match": {"timestamp": {"$gte" : start_time}}},
+        {
+            "$project" : {
+                "_id" : "$_id",
+                "class_id": 1,
+                "dt" : {"$add": [datetime.datetime(1970, 1, 1, 0, 0, 0), "$timestamp"]}
+            },
+        },
+        {
+            "$project" : {
+                "_id" : "$_id",
+                "class_id": 1,
+                "hour" : {"$hour": {"date": "$dt", "timezone": tz_str}}
+            },
+        },    
+        {"$unwind": "$class_id" },
+        {"$group": { "_id": {"hour": "$hour", "class": "$class_id"}, "count": { "$sum": 1 }}}
+    ]
+    try:
+        count = list(db.alerts.aggregate(pipeline))
+        data = {}
+        for cn in count:
+            cls_id = cn["_id"]["class"]
+            if not cls_id in data:
+                data[cls_id] = [0] * 5
+            if cn["_id"]["hour"] in labels:
+            	data[cls_id][labels.index(cn["_id"]["hour"])] = cn["count"]
+        #print(result)
+        return dumps({"labels": labels, "data": data})
+    except:
+        traceback.print_exc()
+        return jsonify(error="Internal server error"), 500
+
+
+def get_hour(hour):
+    return (24 + hour) if (hour < 0) else hour
 
 
 @app.route("/api/v1/alerts", methods=['POST'])
@@ -32,7 +110,7 @@ def create_alerts():
         resp = Response({"Json format error"}, status=400, mimetype='application/json')
         return resp
     try:
-        record_created = mongo.db.alerts.insert_one(content)
+        record_created = db.alerts.insert_one(content)
         return jsonify(id=str(record_created.inserted_id)), 201
     except:
         #traceback.print_exc()
@@ -46,12 +124,16 @@ def get_alerts():
     """
     start = request.args.get('page', 1)
     limit = request.args.get('per_page', 5)
-    if not start.isdigit() or not limit.isdigit():
-        abort(404)
+    #print(isinstance(start, int), limit)
+    try:
+        start = int(start)
+        limit = int(limit)
+    except:
+        abort(400)
     try:
         #alts = list(mongo.db.alerts.find().sort('timestamp', -1).limit(50))
         alts = get_paginated_alerts(request.base_url, int(start), int(limit))
-        print(alts)
+        #print(alts)
         return dumps(alts)
     except:
         traceback.print_exc()
@@ -59,15 +141,18 @@ def get_alerts():
 
 
 def get_paginated_alerts(url, start, limit):
-    result = list(mongo.db.alerts.find().skip((start-1)*limit).limit(limit).sort('timestamp', -1))
-    count = mongo.db.alerts.find().count()
-    if start > count or start < 1:
+    result = list(db.alerts.find().skip((start-1)*limit).limit(limit).sort('timestamp', -1))
+    count = db.alerts.find().count()
+    if start < 1:
         abort(404)
     # construct response
     obj = {}
     obj['current_page'] = start
     obj['per_page'] = limit
     obj['total'] = count
+    obj['from'] = (start - 1) * limit + 1
+    obj['to'] = start * limit
+    obj['last_page'] = math.ceil(count/limit)
     # make URLs
     if start == 1:
         obj['prev_page_url'] = ''
@@ -80,7 +165,35 @@ def get_paginated_alerts(url, start, limit):
         obj['next_page_url'] = url + '?page=%d&per_page=%d' % (start + 1, limit)
     obj['data'] = result
     return obj
-    
+
+
+def notification():
+    try:
+        #for insert_change in db.collection.alerts.watch([{'$match': {'operationType': 'insert'}}]):
+        with app.app_context():
+            for insert_change in db.alerts.watch([{'$match': {'operationType': 'insert'}}]):
+                #print(insert_change)
+                sse.publish(dumps(insert_change), type="notification")
+        return "Notification sent"
+    except PyMongoError:
+        # We know it's unrecoverable: todo resume connection
+        print('connection closed unexpectedly.') 
+
+
+@app.route('/video', methods=['GET'])
+def media():
+    # todo: take camera id as param
+    camID = request.args.get('cam', 1)
+    ts = request.args.get('ts', "")
+    return send_file("video/cam{}_{}.mp4".format(camID, ts))
+
+
+@app.before_first_request
+def task():
+    print("before request.")
+    executor.submit(notification)
+    return "One job was launched in background!"  
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
